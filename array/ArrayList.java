895f61a0bf32a4b5428da5240ee0db25c0c3b3c9
souravcodes1080 <techburnt06@gmail.com>
2023-08-08 22:21:21 +0530

    arraylist

---
+++ array/ArrayList.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class ArrayList{
+	public static void main(String[] args){
+		//int[] a = new int[5];
+		ArrayList<Integer> a = new ArrayList<>();
+		System.out.println(a);
+	}
+}
